8b679d63f1020e68ff34413207fd7027782cc33f
ramror3 <rameshror3@gmail.com>
2017-05-16 15:23:42 +0530

    This is my final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { AgmCoreModule } from 'angular2-google-maps/core';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,6 +12,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDTzIt6K0uWMmgzuQfhIRztu7QutfmcBP4'
+    }),
     HttpModule
   ],
   providers: [],
